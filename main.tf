aec50d44362d0849b86c37a519cc4a6eb682b899
IshaanBoseHC <141964041+IshaanBoseHC@users.noreply.github.com>
2025-06-12 09:49:57 -0700

    Adding 5 new resources, changing 1 of the pet resources, and deleting one

--- main.tf
+++ main.tf
@@ -1,12 +1,53 @@
 terraform {
   required_version = ">= 1.0.0"
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1.0"
+    }
+  }
 }
 
 resource "random_pet" "example" {
-  length = 2
+  length = 4  # Modified the length of the random pet
 }
 
-resource "null_resource" "example" {
+resource "null_resource" "example_2" {  # Renamed the existing null resource
+  triggers = {
+    pet_name = random_pet.example.id
+  }
+}
+
+# New null resources
+resource "null_resource" "new_1" {
+  triggers = {
+    pet_name = random_pet.example.id
+  }
+}
+
+resource "null_resource" "new_2" {
+  triggers = {
+    pet_name = random_pet.example.id
+  }
+}
+
+resource "null_resource" "new_3" {
+  triggers = {
+    pet_name = random_pet.example.id
+  }
+}
+
+resource "null_resource" "new_4" {
+  triggers = {
+    pet_name = random_pet.example.id
+  }
+}
+
+resource "null_resource" "new_5" {
   triggers = {
     pet_name = random_pet.example.id
   }
