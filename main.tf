b791514f82be1c1f3ddc59491f2fe37b56483aa2
IshaanBoseHC <141964041+IshaanBoseHC@users.noreply.github.com>
2025-06-12 09:51:12 -0700

    Fixing errors

--- main.tf
+++ main.tf
@@ -1,22 +1,12 @@
 terraform {
   required_version = ">= 1.0.0"
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = ">= 3.1.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.1.0"
-    }
-  }
 }
 
 resource "random_pet" "example" {
-  length = 4  # Modified the length of the random pet
+  length = 4
 }
 
-resource "null_resource" "example_2" {  # Renamed the existing null resource
+resource "null_resource" "example_2" {
   triggers = {
     pet_name = random_pet.example.id
   }
