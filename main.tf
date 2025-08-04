ff88ea833e1024b5312c212d3dea0d4203b75cca
IshaanBoseHC <141964041+IshaanBoseHC@users.noreply.github.com>
2025-08-04 10:48:50 -0700

    adding resources

--- main.tf
+++ main.tf
@@ -70,3 +70,15 @@ resource "null_resource" "new_6" {
   }
 }
 
+
+
+resource "aws_instance" "server" {
+  count = 4 # create four similar EC2 instances
+
+  ami           = "ami-a1b2c3d4"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Server ${count.index}"
+  }
+}
